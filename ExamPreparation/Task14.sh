1ca1340c668fc34f4047eb70eec4303aec6020c6
Madburn <mitang31@abv.bg>
2020-08-28 23:42:29 +0300

    Update Task14.sh

--- ExamPreparation/Task14.sh
+++ ExamPreparation/Task14.sh
@@ -25,3 +25,44 @@ do
     fi
 done
 
+OR
+
+#!/bin/bash
+  
+if [[ $# -ne 1 ]]; then
+    echo "Please enter 1 argument!"
+    exit 1
+fi
+
+num=$1
+
+if [[ ! $num =~ [0-9]+ ]]; then
+    echo "Please enter a number!"
+    exit 2
+fi
+
+#if [[ 'whoami' != 'root' ]]; then
+#   echo "Permission denied! You must be root to run this script!"
+#   exit 3
+#fi
+
+userList=$(mktemp)
+
+ps -eo user | sort | uniq > ${userList}
+
+while read line; do
+
+userTotalRSS=$(ps -eo user,rss | grep "${line}" | tr -s [:blank:] | sort -t ' ' -k 2 -n | awk '{sum += $2} END {print sum}')
+maxRSSPID=$(ps -eo user,pid,rss | grep "${line}" | tr -s [:blank:] | sort -t ' ' -k3 -n | tail -n 1 | cut -d ' ' -f2)
+
+echo "${line} : ${userTotalRSS}"
+
+if [[ ${userTotalRSS} -gt ${num} ]]; then
+    #kill -15 ${maxRSSPID}
+    sleep 0
+    #kill -9 ${maxRSSPID}
+fi
+
+done < "${userList}"
+
+rm -- "${userList}"
